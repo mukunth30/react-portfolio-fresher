f87fea91d532ca49960b38c6e482bc7e36bb2143
MUKUNTHA RAJ <146414128+mukunth30@users.noreply.github.com>
2024-01-23 15:09:07 +0530

    Update AboutContant.jsx

--- src/compontants/AboutContant.jsx
+++ src/compontants/AboutContant.jsx
@@ -9,7 +9,7 @@ const AboutContant = () => {
         <div className="left">
             <div className='abt-me'>
                 <h1>WHO AM I ?</h1>
-            <p>Hello! 👋 I'm a fresh React.js front-end developer with a background in marketing management. I hold a B.Com degree in Marketing Management, and I've recently acquired skills in front-end development through my studies at Aimore Technology. Passionate about creating intuitive and user-friendly web interfaces, I'm excited to contribute my skills to the dynamic world of web development. Let's build something amazing together! 🚀</p>
+            <p>Hey there! 👋 I'm Mukunth, a dedicated front-end developer with one year of experience in HTML, CSS, JavaScript, React.js, Bootstrap, and version control with Git and GitHub. I am passionate about creating seamless and visually appealing web experiences. I thrive on staying ahead of industry trends, and my collaborative spirit ensures smooth teamwork, utilizing Git for effective version control. With a user-centric approach, I aim to craft engaging and intuitive interfaces. Let's collaborate and bring innovative web solutions to life! 🚀</p>
             </div>
             <div className="skills">
             <h1>SKILLS</h1>
@@ -48,20 +48,16 @@ const AboutContant = () => {
                 />
             </h4>
             </div>
-            <div className='lol'>
-                <h1>EDUCATION</h1>
-            </div>
             <div className="education">
-                <div className="clg">
-                <h2>COLLEGE:</h2>
-                <h3>AGRUCHAND MANMUKK JAIN COLLEGE-CHENNAI</h3>
-                <p>B.COM(MARKETING MANAGEMENT)-2022</p>
-                </div>
-                <div className="course">
-                <h2>CERTIFICATE:</h2>
-                <h3>AIMORE TECHNOLOGY-CHENNAI</h3>
-                <p>REACT FRONT-END DEVELOPER-2022</p>
-                </div>
+              <h1>EDUCATION</h1>
+              <h2>AGRUCHAND MANULL JAIN COLLEGE</h2>
+              <p>B.COM(MAKKETING MANAGEMENT)-2022</p>
+            </div>
+            <div className="exp">
+              <h1>EXPERIENCE</h1>
+              <h2>APTIVE TECHNOLOGY</h2>
+              <h5>FRONT-END DEVELOPER -0CT(2022-2023)</h5>
+              <p>With over a year of dedicated experience as a front-end developer at Aptive Technology, I have honed my skills in HTML, CSS, and JavaScript, establishing a solid foundation for web development. My proficiency extends to React.js, where I have a strong grasp of fundamental concepts and have delved into more advanced features such as hooks and context API. Throughout my tenure, I have actively contributed to the development of responsive and visually appealing user interfaces. I am committed to staying abreast of industry trends, continuously expanding my knowledge base, and embracing emerging technologies. Beyond technical skills, my experience has cultivated effective collaboration and communication skills, enabling me to work seamlessly within development teams. As I pursue excellence in my field, I am passionate about crafting optimal user experiences and contributing to innovative and impactful projects in the dynamic landscape of front-end development.</p>
             </div>
         </div>
     </div>
